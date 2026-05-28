9086edc847fcdafe700e8299d9a3bc9196703787
ludovico.audenino <ludovico.aundenino@gmail.com>
2026-05-28 17:42:03 +0200

    refactor: modularize operator main loop and service handlers into dedicated functions

--- src/operatore.c
+++ src/operatore.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/sem.h>
+#include <time.h>
 
 #define JOLLY_MSG_TYPE 63
 #define ORDER_PRIMI_TYPE 1
@@ -31,6 +32,14 @@ typedef struct {
   int served;
   pid_t pid;
 } ServingMsg;
+
+typedef struct {
+  long mytype;
+  int primi_ordered;
+  int secondi_ordered;
+  int coffee_ordered;
+  pid_t pid;
+} CassaMsg;
 
 typedef struct {
   long mytype;
@@ -95,6 +104,80 @@ static void set_StationProperties(struct SharedData *shm) {
   }
 }
 
+static void serve_primi_secondi(struct SharedData *shm, ServingMsg *order) {
+  int dish_type = order->dish_type;
+  int served = 0;
+  int portion_mutex =
+      (target_station == 0) ? SEM_MUTEX_PORZIONI_P : SEM_MUTEX_PORZIONI_S;
+
+  sem_op(sem_id, portion_mutex, -1, SEM_UNDO);
+  if (target_station == 0) {
+    if (shm->portion_left_primi[dish_type] > 0) {
+      shm->portion_left_primi[dish_type]--;
+      served = 1;
+      sem_op(sem_id, SEM_MUTEX_STATS, -1, SEM_UNDO);
+      shm->sim_stats.dishes_primi_today++;
+      sem_op(sem_id, SEM_MUTEX_STATS, +1, SEM_UNDO);
+    }
+  } else {
+    if (shm->portion_left_secondi[dish_type] > 0) {
+      shm->portion_left_secondi[dish_type]--;
+      served = 1;
+      sem_op(sem_id, SEM_MUTEX_STATS, -1, SEM_UNDO);
+      shm->sim_stats.dishes_secondi_today++;
+      sem_op(sem_id, SEM_MUTEX_STATS, +1, SEM_UNDO);
+    }
+  }
+  sem_op(sem_id, portion_mutex, +1, SEM_UNDO);
+
+  ServingMsg reply;
+  reply.mytype = order->pid;
+  reply.served = served;
+  send_message(msg_id, &reply, MSG_CONTENT_SIZE(ServingMsg), 0);
+}
+
+static void serve_caffe(struct SharedData *shm, ServingMsg *order) {
+  ServingMsg reply;
+  reply.mytype = order->pid;
+  reply.served = 1;
+
+  send_message(msg_id, &reply, MSG_CONTENT_SIZE(ServingMsg), 0);
+  sem_op(sem_id, SEM_MUTEX_STATS, -1, SEM_UNDO);
+  shm->sim_stats.dishes_coffee_today++;
+  sem_op(sem_id, SEM_MUTEX_STATS, +1, SEM_UNDO);
+}
+
+static void serve_cassa(struct SharedData *shm, ServingMsg *order) {}
+
+static void handle_jolly_assignment(struct SharedData *shm) {
+  if (target_station == -1) {
+    JollyMsg jolly_msg;
+    receive_message(msg_id, &jolly_msg, MSG_CONTENT_SIZE(JollyMsg),
+                    JOLLY_MSG_TYPE, 0);
+    target_station = jolly_msg.station_id;
+    set_StationProperties(shm);
+  }
+}
+
+static void run_workday(struct SharedData *shm) {
+  while (shm->day_running) {
+    ServingMsg order;
+    receive_message(msg_id, &order, MSG_CONTENT_SIZE(ServingMsg),
+                    station_prop.msg_type, 0);
+    int service_time =
+        random_service_time(station_prop.avg_srvc, station_prop.srvc_delta);
+    sim_sleep(service_time, shm->config.n_nano_secs);
+
+    if (target_station == 0 || target_station == 1) {
+      serve_primi_secondi(shm, &order);
+    } else if (target_station == 2) {
+      serve_caffe(shm, &order);
+    } else if (target_station == 3) {
+      serve_cassa(shm, &order);
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
   (void)argc;
   (void)argv;
@@ -111,68 +194,19 @@ int main(int argc, char *argv[]) {
     sem_op(sem_id, SEM_READY, +1, 0);
     sem_op(sem_id, SEM_DAY_START, -1, 0);
 
-    if (target_station == -1) {
-      JollyMsg jolly_msg;
-      receive_message(msg_id, &jolly_msg, MSG_CONTENT_SIZE(JollyMsg),
-                      JOLLY_MSG_TYPE, 0);
-      target_station = jolly_msg.station_id;
-      set_StationProperties(shm);
-    }
+    handle_jolly_assignment(shm);
+
     if (target_station >= 0 && target_station <= 3) {
       int target_sem = station_prop.sem_seats_index;
       sem_op(sem_id, target_sem, -1, SEM_UNDO);
     }
 
-    while (shm->day_running) {
-      ServingMsg order;
-      receive_message(msg_id, &order, MSG_CONTENT_SIZE(ServingMsg),
-                      station_prop.msg_type, 0);
-      int service_time =
-          random_service_time(station_prop.avg_srvc, station_prop.srvc_delta);
-      sim_sleep(service_time, shm->config.n_nano_secs);
+    run_workday(shm);
 
-      if (target_station == 0 || target_station == 1) {
-        int dish_type = order.dish_type;
-        int served = 0;
-        int portion_mutex =
-            (target_station == 0) ? SEM_MUTEX_PORZIONI_P : SEM_MUTEX_PORZIONI_S;
-
-        sem_op(sem_id, portion_mutex, -1, SEM_UNDO);
-        if (target_station == 0) {
-          if (shm->portion_left_primi[dish_type] > 0) {
-            shm->portion_left_primi[dish_type]--;
-            served = 1;
-            sem_op(sem_id, SEM_MUTEX_STATS, -1, SEM_UNDO);
-            shm->sim_stats.dishes_primi_today++;
-            sem_op(sem_id, SEM_MUTEX_STATS, +1, SEM_UNDO);
-          }
-        } else {
-          if (shm->portion_left_secondi[dish_type] > 0) {
-            shm->portion_left_secondi[dish_type]--;
-            served = 1;
-            sem_op(sem_id, SEM_MUTEX_STATS, -1, SEM_UNDO);
-            shm->sim_stats.dishes_secondi_today++;
-            sem_op(sem_id, SEM_MUTEX_STATS, +1, SEM_UNDO);
-          }
-        }
-        sem_op(sem_id, portion_mutex, +1, SEM_UNDO);
-
-        ServingMsg reply;
-        reply.mytype = order.pid;
-        reply.served = served;
-        send_message(msg_id, &reply, MSG_CONTENT_SIZE(ServingMsg), 0);
-      } else if (target_station == 2) {
-        ServingMsg reply;
-        reply.mytype = order.pid;
-        reply.served = 1;
-
-        send_message(msg_id, &reply, MSG_CONTENT_SIZE(ServingMsg), 0);
-        sem_op(sem_id, SEM_MUTEX_STATS, -1, SEM_UNDO);
-        shm->sim_stats.dishes_coffee_today++;
-        sem_op(sem_id, SEM_MUTEX_STATS, +1, SEM_UNDO);
-      }
+    if (target_station >= 0 && target_station <= 3) {
+      int target_sem = station_prop.sem_seats_index;
+      sem_op(sem_id, target_sem, +1, SEM_UNDO);
     }
-    sem_op(sem_id, station_prop.sem_seats_index, +1, SEM_UNDO);
   }
   return 0;
 }
